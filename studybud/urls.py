872ca79ef3c6261c17d9d51df14011050a9272c2
zekiunyildiz36 <zeki.unyildiz@alo-tech.com>
2023-11-06 01:16:32 +0300

    temel template olusturdum

--- studybud/urls.py
+++ studybud/urls.py
@@ -1,7 +1,10 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.http import HttpResponse
+from django.urls import include, path
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('base.urls'))
 ]
